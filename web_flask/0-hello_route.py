36b1a5476389fd96e5f07cd1e6f125b4f0a0a9bc
PMagombedze <percymagoras@outlook.com>
2023-10-21 14:39:06 +0200

    slashes

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,10 +3,13 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
 @app.route('/')
 def home():
     return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, strict_slashes=False)
