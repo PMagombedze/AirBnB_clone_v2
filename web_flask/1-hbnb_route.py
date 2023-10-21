34154c17b4a36ad114c1b269e5a5db4aaf59f8f9
PMagombedze <percymagoras@outlook.com>
2023-10-21 14:43:23 +0200

    hbnb route

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,6 +10,10 @@ app.url_map.strict_slashes = False
 def home():
     return 'Hello HBNB!'
 
+@app.route('/hbnb')
+def hbnb():
+    return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
